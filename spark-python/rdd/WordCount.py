774008d5220dba4b7f95a8e5760268d10a28dee4
AllenSun7 <sunlun7@yahoo.com>
2019-12-12 12:53:08 -0800

    try test

---
+++ spark-python/rdd/WordCount.py
@@ -0,0 +1,17 @@
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("word count").setMaster("local[3]")
+    sc = SparkContext(conf = conf)
+    
+    lines = sc.textFile("in/1.")
+    
+    words = lines.flatMap(lambda line: line.split(" "))
+    
+    wordCounts = words.countByValue()
+    
+    for word, count in wordCounts.items():
+        if word == word[::-1]:
+            print("True!!")
+        print("{} : {}".format(word, count))
+
