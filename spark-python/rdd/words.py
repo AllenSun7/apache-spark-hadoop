63e89cd88ea790715a558a0aebc6af3cd6491d98
AllenSun7 <sunlun7@yahoo.com>
2019-12-14 10:38:27 -0800

    format

--- spark-python/rdd/words.py
+++ spark-python/rdd/words.py
@@ -7,17 +7,18 @@ import timeit
 
 
 def word_count():
-    # count words of palindrome and anagram respectively
+    '''count words of palindrome and anagram respectively'''
     conf = SparkConf().setAppName("Apache spark").setMaster("local[6]")
     sc = SparkContext(conf = conf)
     parent_path = "in/maildir/allen-p"
     names = get_immediate_subdirectories(parent_path)
     dic_palindrome = {}
     dic_anagram = {}
-    invalid_path = []
-    #lines = sc.textFile("in/test-data/*")
+    invalid_path = [] # some subdirectories are not readable
+    #count = 1
     for name in names:
-        print(names)
+        #print("It is the %dth number of %d" % (count,len(names)))
+        #count += 1
         lines_path = parent_path + "/" + name + "/*"
         try:
             lines = sc.textFile(lines_path) 
@@ -41,14 +42,6 @@ def word_count():
     group_dic_anagram = group_anagram(dic_anagram)
     print(group_dic_anagram)
     print("================================================================")
-    
-    #lines = sc.textFile("in/maildir/*") #all dataset
-    
-    #lines = sc.textFile("in/maildir/arnold-j/*") # less data
-
-    #lines = sc.textFile("in/allen-p/_sent_mail/*") # partial data
-    #lines = sc.textFile("in/test-data/*") # test data   
-
 
 def merge_dict(dict1, dict2):
    ''' Merge dictionaries and keep values of common keys in list'''
@@ -60,7 +53,7 @@ def merge_dict(dict1, dict2):
    return dict3
 
 def words_filter(wordCounts):
-    #filt words
+    '''filt words'''
     d = enchant.Dict("en_US") #english word
     contents_anagram = list_anagram()
     dic_anagram = {}
@@ -79,25 +72,22 @@ def words_filter(wordCounts):
                     if get_anagram(word, count, contents_anagram):  
                         dic_anagram.update(word_dic)
                         
-    #print(dic_palindrome)
-    #group_dic_anagram = group_anagram(dic_anagram)
-    #print(group_dic_anagram)
     return (dic_palindrome, dic_anagram)
 
 def get_palindrome(word, count):
-    #palindrome
+    '''palindrome'''
     if word == word[::-1]:  
         #not same characters: "AAA","BBB"...
         if not all(c == word[0] for c in word[1:]):
             return True
 
 def get_anagram(word, count, contents_anagram):
-    #anagram
+    '''anagram'''
     if word in contents_anagram:
         return True
 
 def list_anagram():
-    #read the file of all anagrams and store into array
+    '''read the input file of all anagrams and store into array'''
     #get absolute path
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
     my_file = os.path.join(THIS_FOLDER, 'anagrams.txt')
@@ -106,8 +96,7 @@ def list_anagram():
     return array
 
 def group_anagram(dic_anagram):
-    # using defaultdict() + sorted() + values() 
-    # Grouping Anagrams 
+    '''Grouping Anagrams by using defaultdict() + sorted() + values()'''   
     test_list = [{key: value} for key, value in dic_anagram.items()]
     temp = defaultdict(list) 
     for ele in test_list: 
@@ -116,17 +105,17 @@ def group_anagram(dic_anagram):
     res = list(temp.values())   
     return str(res)
 
-#get subdiretory 
 def get_immediate_subdirectories(a_dir):
+    '''get subdiretory of dataset'''
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 
 def main():
-    #cauculate runtime
+    '''cauculate runtime'''
     start = timeit.default_timer()
     word_count()
     stop = timeit.default_timer()
-    print('Time: ', stop - start) 
+    print('Runtime: ', stop - start) 
 
 if __name__ == "__main__":
     main()
