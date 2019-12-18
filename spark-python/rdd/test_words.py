813ae40297a512af87235cd0f27080f55dee503d
AllenSun7 <sunlun7@yahoo.com>
2019-12-18 00:24:40 -0800

    gcp

--- spark-python/rdd/test_words.py
+++ spark-python/rdd/test_words.py
@@ -56,7 +56,9 @@ def lenth_words(word):
 
 def merge_dict(dict1, dict2):
    ''' Merge dictionaries and keep values of common keys in list'''
-   dict3 = {**dict1, **dict2}
+   dict3 = {}
+   dict3.update(dict1)
+   dict3.update(dict2)
    for key, value in dict3.items():
        if key in dict1 and key in dict2:
                dict3[key] = int(value) + int (dict1[key])
